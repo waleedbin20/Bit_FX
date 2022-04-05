97ebc7fd1598805cff2f61d40b99f59cfa0aa3aa
Waleed Bin Asad <waleedbinasad18@gmail.com>
2022-04-05 22:39:46 +0100

    calculator

--- bitfx_application/lib/ui/views/calculator/calculator_view.dart
+++ bitfx_application/lib/ui/views/calculator/calculator_view.dart
@@ -18,167 +18,141 @@ class _CalculatorViewState extends State<CalculatorView> {
   @override
   Widget build(BuildContext context) {
     return ViewModelBuilder<CalculatorViewModel>.reactive(
-        viewModelBuilder: () => CalculatorViewModel(),
-        onModelReady: (viewModel) => viewModel.calculate(),
-        builder: (context, viewModel, child) => SafeArea(
-              child: Scaffold(
-                  appBar: AppBar(
-                    backgroundColor: mainGrey,
-                    title: Center(
-                        child: Text(
-                      "FX Calculator",
-                      style: TextStyle(
-                          fontSize: 30,
-                          fontWeight: FontWeight.bold,
-                          color: mainCyan),
-                    )),
-                    automaticallyImplyLeading: false,
-                  ),
-                  body: Container(
-                      height: MediaQuery.of(context).size.height,
-                      width: MediaQuery.of(context).size.width,
-                      decoration: BoxDecoration(color: Colors.black),
-                      child: SingleChildScrollView(
-                        child: Padding(
-                          padding: const EdgeInsets.all(20.0),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              SizedBox(
-                                  height: MediaQuery.of(context).size.height *
-                                      0.02),
-                              CalculatorTextBox(
-                                controller: viewModel.accBal,
-                                text: 'Account Balance',
-                              ),
-                              SizedBox(
-                                  height: MediaQuery.of(context).size.height *
-                                      0.01),
-                              MyStatefulWidget(),
-                              SizedBox(
-                                  height: MediaQuery.of(context).size.height *
-                                      0.01),
-                              CalculatorTextBox(
-                                controller: viewModel.riskP,
-                                text: 'Risk Percent',
-                              ),
-                              SizedBox(
-                                  height: MediaQuery.of(context).size.height *
-                                      0.01),
-                              CalculatorTextBox(
-                                controller: viewModel.stoploss,
-                                text: 'Stop Loss',
-                              ),
-                              SizedBox(
-                                  height:
-                                      MediaQuery.of(context).size.height * 0.1),
-                              Button(
-                                  textValue: "Calculate",
-                                  onPressed: () {
-                                    viewModel.calculate();
-                                    viewModel.clearTextInput();
-                                  }),
-
-                              Padding(
-                                  padding: const EdgeInsets.only(top:30) ,
-                                  child: Text(viewModel.result,
-                                      style: TextStyle(
-                                          color: mainCyan, fontSize: 20)))
-                            ],
-                          ),
-                        ),
-                      ))),
-            ));
-  }
-}
-
-//DropDown Box for Forex Pair
-class MyStatefulWidget extends StatefulWidget {
-  const MyStatefulWidget({Key? key}) : super(key: key);
-
-  @override
-  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
-}
-
-class _MyStatefulWidgetState extends State<MyStatefulWidget> {
-  String dropdownValue = 'EURUSD';
-
-  @override
-  Widget build(BuildContext context) {
-    return CurrencyPair(context);
-  }
-
-  Container CurrencyPair(BuildContext context) {
-    return Container(
-        width: MediaQuery.of(context).size.width * 0.8,
-        height: MediaQuery.of(context).size.height * 0.1,
-        child: FittedBox(
-          child: Card(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(15.0),
-            ),
-            color: mainCyan,
-            elevation: 4.0,
-            child: Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Icon(
-                    Icons.attach_money_outlined,
-                    size: 30,
-                  ),
-                  Text("Select Pair",
-                      style: TextStyle(
-                          fontSize: 20.0,
-                          height: 1.0,
-                          color: Colors.black,
-                          fontWeight: FontWeight.w500)),
-                  SizedBox(width: 40),
-                  Row(
-                    children: [
-                      Container(
-                        width: 70.0,
-                        height: 30,
-                        child: SizedBox(
-                          child: DropdownButton<String>(
-                            menuMaxHeight: 600,
-                            isExpanded: true,
-                            dropdownColor: Color.fromARGB(255, 208, 255, 249),
-                            value: dropdownValue,
-                            icon: const Icon(
-                              Icons.arrow_downward,
-                              color: Colors.black,
-                            ),
-                            iconSize: 10,
-                            elevation: 16,
-                            style: const TextStyle(color: Colors.black),
-                            onChanged: (String? newValue) {
-                              setState(() {
-                                dropdownValue = newValue!;
-                              });
-                            },
-                            items: <String>[
-                              'EURUSD',
-                              'GBPUSD',
-                              'NZDUSD',
-                              'XAUUSD',
-                              'GBPJPY',
-                            ].map<DropdownMenuItem<String>>((String value) {
-                              return DropdownMenuItem<String>(
-                                value: value,
-                                child: Text(value),
-                              );
-                            }).toList(),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
+      viewModelBuilder: () => CalculatorViewModel(),
+      onModelReady: (viewModel) => viewModel.calulateAccordingToPairs(),
+      builder: (context, viewModel, child) => SafeArea(
+        child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: mainGrey,
+            title: Center(
+                child: Text(
+              "FX Calculator",
+              style: TextStyle(
+                  fontSize: 30, fontWeight: FontWeight.bold, color: mainCyan),
+            )),
+            automaticallyImplyLeading: false,
+          ),
+          body: Container(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(color: Colors.black),
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    SizedBox(height: MediaQuery.of(context).size.height * 0.02),
+                    CalculatorTextBox(
+                      controller: viewModel.accBal,
+                      text: 'Account Balance',
+                    ),
+                    SizedBox(height: MediaQuery.of(context).size.height * 0.01),
+                    BuildDropDown(),
+                    SizedBox(height: MediaQuery.of(context).size.height * 0.01),
+                    CalculatorTextBox(
+                      controller: viewModel.riskP,
+                      text: 'Risk Percent',
+                    ),
+                    SizedBox(height: MediaQuery.of(context).size.height * 0.01),
+                    CalculatorTextBox(
+                      controller: viewModel.stoploss,
+                      text: 'Stop Loss',
+                    ),
+                    SizedBox(height: MediaQuery.of(context).size.height * 0.1),
+                    Button(
+                        textValue: "Calculate",
+                        onPressed: () {
+                          viewModel.calulateAccordingToPairs();
+                          viewModel.clearTextInput();
+                        }),
+                    Padding(
+                        padding: const EdgeInsets.only(top: 30),
+                        child: Text(viewModel.result,
+                            style: TextStyle(color: mainCyan, fontSize: 20)))
+                  ],
+                ),
               ),
             ),
           ),
-        ));
+        ),
+      ),
+    );
+  }
+}
+
+class BuildDropDown extends ViewModelWidget<CalculatorViewModel> {
+  @override
+  Widget build(BuildContext context, CalculatorViewModel viewModel) {
+    return Container(
+      width: MediaQuery.of(context).size.width * 0.8,
+      height: MediaQuery.of(context).size.height * 0.1,
+      child: FittedBox(
+        child: Card(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15.0),
+          ),
+          color: mainCyan,
+          elevation: 4.0,
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Icon(
+                  Icons.attach_money_outlined,
+                  size: 30,
+                ),
+                Text("Select Pair",
+                    style: TextStyle(
+                        fontSize: 20.0,
+                        height: 1.0,
+                        color: Colors.black,
+                        fontWeight: FontWeight.w500)),
+                SizedBox(width: 40),
+                Row(
+                  children: [
+                    Container(
+                      width: 70.0,
+                      height: 30,
+                      child: SizedBox(
+                        child: DropdownButton<String>(
+                          menuMaxHeight: 600,
+                          isExpanded: true,
+                          dropdownColor: Color.fromARGB(255, 208, 255, 249),
+                          value: viewModel.dropdownValue,
+                          icon: const Icon(
+                            Icons.arrow_downward,
+                            color: Colors.black,
+                          ),
+                          iconSize: 10,
+                          elevation: 16,
+                          style: const TextStyle(color: Colors.black),
+                          onChanged: (String? newValue) {
+                            viewModel.dropdownValue = newValue!;
+                          },
+                          items: <String>[
+                            'EURUSD',
+                            'GBPUSD',
+                            'NZDUSD',
+                            'XAUUSD',
+                            'GBPJPY',
+                          ].map<DropdownMenuItem<String>>((String value) {
+                            return DropdownMenuItem<String>(
+                              value: value,
+                              child: Text(value),
+                            );
+                          }).toList(),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
--- bitfx_application/lib/viewmodels/calculator/calculator_view_model.dart
+++ bitfx_application/lib/viewmodels/calculator/calculator_view_model.dart
@@ -6,26 +6,62 @@ class CalculatorViewModel extends ChangeNotifier {
   TextEditingController riskP = TextEditingController();
   TextEditingController stoploss = TextEditingController();
 
-  
   double _rslt = 0;
+
   String _result = '';
+
   String get result => _result;
 
-  void calculate() {
+  String _dropdownValue = 'EURUSD';
+
+  String get dropdownValue => _dropdownValue;
+
+  set dropdownValue(String value) {
+    _dropdownValue = value;
+    notifyListeners();
+  }
+
+  void calculate(int pipSize) {
     double? x = double.tryParse(accBal.text) ?? 0;
     double? y = double.tryParse(riskP.text) ?? 0;
     double? z = double.tryParse(stoploss.text) ?? 0;
     double r = y * 0.01;
-    _rslt = (x * r / z) / 10;
+    _rslt = (x * r / z) / pipSize;
     _result = ('Lot Size: ' + _rslt.toStringAsFixed(2));
     notifyListeners();
   }
 
-  clearTextInput(){
- 
+  void calulateAccordingToPairs() {
+    var forexPair = dropdownValue;
+    switch (forexPair) {
+      case 'EURUSD':
+      case 'GBPUSD':
+      case 'NZDUSD':
+        {
+          calculate(10);
+        }
+        break;
+      case 'XAUUSD':
+        {
+          calculate(10);
+        }
+        break;
+      case 'GBPJPY':
+        {
+          calculate(1000);
+        }
+        break;
+      default:
+        {
+          print("Invalid choice");
+        }
+        break;
+    }
+  }
+
+  clearTextInput() {
     accBal.clear();
     riskP.clear();
     stoploss.clear();
- 
   }
 }
